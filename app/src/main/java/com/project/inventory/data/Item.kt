3f434d0c1466a9cbf11643a1788ea6561abd21e4
Muhammed Danish <danishmuhammed01@gmail.com>
2023-09-07 21:48:57 +0530

    create an item entity

--- app/src/main/java/com/project/inventory/data/Item.kt
+++ app/src/main/java/com/project/inventory/data/Item.kt
@@ -1,7 +1,11 @@
 package com.project.inventory.data
 
-class Item(
-    val id: Int = 0,
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "items")
+data class Item(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val name: String,
     val price: Double,
     val quantity: Int
